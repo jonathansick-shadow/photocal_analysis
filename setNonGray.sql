30ff29192f721f0c2343dc80e569171ab9655d23
taxelrod <taxelrod@git.lsstcorp.org>
2008-10-30 15:34:48 +0000

    Used extinction coeffs from Allyn Smith Nov, 2006 paper

--- setNonGray.sql
+++ setNonGray.sql
@@ -1,5 +1,5 @@
 insert into NonGray_Extinction (ccdExposureId) SELECT Raw_FPA_Exposure.rawFPAExposureId from Raw_FPA_Exposure;
-update NonGray_Extinction as ng, Raw_FPA_Exposure as ex set ng.kNonGray = 0.15*ex.airmass where ng.ccdExposureId=ex.rawFPAExposureId and ex.filterId=1;
-update NonGray_Extinction as ng, Raw_FPA_Exposure as ex set ng.kNonGray = 0.06*ex.airmass where ng.ccdExposureId=ex.rawFPAExposureId and ex.filterId=2;
-update NonGray_Extinction as ng, Raw_FPA_Exposure as ex set ng.kNonGray = 0.06*ex.airmass where ng.ccdExposureId=ex.rawFPAExposureId and ex.filterId=3;
-update NonGray_Extinction as ng, Raw_FPA_Exposure as ex set ng.kNonGray = 0.06*ex.airmass where ng.ccdExposureId=ex.rawFPAExposureId and ex.filterId=4;
+update NonGray_Extinction as ng, Raw_FPA_Exposure as ex set ng.kNonGray = 0.181*ex.airmass where ng.ccdExposureId=ex.rawFPAExposureId and ex.filterId=1;
+update NonGray_Extinction as ng, Raw_FPA_Exposure as ex set ng.kNonGray = 0.095*ex.airmass where ng.ccdExposureId=ex.rawFPAExposureId and ex.filterId=2;
+update NonGray_Extinction as ng, Raw_FPA_Exposure as ex set ng.kNonGray = 0.089*ex.airmass where ng.ccdExposureId=ex.rawFPAExposureId and ex.filterId=3;
+update NonGray_Extinction as ng, Raw_FPA_Exposure as ex set ng.kNonGray = 0.053*ex.airmass where ng.ccdExposureId=ex.rawFPAExposureId and ex.filterId=4;
