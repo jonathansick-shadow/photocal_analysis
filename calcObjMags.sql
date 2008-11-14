19814297bacd7f3ff0e4f622a3b4739c174885ad
taxelrod <taxelrod@git.lsstcorp.org>
2008-11-14 22:22:49 +0000

    Changed to use avg(calibrated mag) instead of min(instrumental mag)

--- calcObjMags.sql
+++ calcObjMags.sql
@@ -1,4 +1,4 @@
-update Object o set gMag = (SELECT min(psfMag) from DIASource ds WHERE ds.objectId = o.objectId and ds.filterId = 1);
-update Object o set rMag = (SELECT min(psfMag) from DIASource ds WHERE ds.objectId = o.objectId and ds.filterId = 2);
-update Object o set iMag = (SELECT min(psfMag) from DIASource ds WHERE ds.objectId = o.objectId and ds.filterId = 3);
-update Object o set zMag = (SELECT min(psfMag) from DIASource ds WHERE ds.objectId = o.objectId and ds.filterId = 4);
+update Object o set gMag = (SELECT avg(apMag) from DIASource ds WHERE ds.objectId = o.objectId and ds.filterId = 1);
+update Object o set rMag = (SELECT avg(apMag) from DIASource ds WHERE ds.objectId = o.objectId and ds.filterId = 2);
+update Object o set iMag = (SELECT avg(apMag) from DIASource ds WHERE ds.objectId = o.objectId and ds.filterId = 3);
+update Object o set zMag = (SELECT avg(apMag) from DIASource ds WHERE ds.objectId = o.objectId and ds.filterId = 4);
