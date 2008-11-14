f1195ab720a82afec1403e11b0106999eaf340b9
taxelrod <taxelrod@git.lsstcorp.org>
2008-11-14 22:24:21 +0000

    Set calibrated mag to extinction corrected mag prior to applying gray surface

--- applyGraySurf.sql
+++ applyGraySurf.sql
@@ -1,1 +1,2 @@
+UPDATE DIASource set apMag=modelMag;
 UPDATE DIASource as s, Gray_Surf as g, DIA_Poly as p SET s.apMag = s.modelMag - (g.c0 + g.cx1*p.valx1 + g.cx2*p.valx2 + g.cy1*p.valy1 + g.cy2*p.valy2 + g.cxy*p.valxy) where g.ccdExposureId = s.ccdExposureId and s.diaSourceId = p.diaSourceId;
